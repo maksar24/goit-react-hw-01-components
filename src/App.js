c9ceaac89da575855f03812398ff35eca5a2c703
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-07-08 11:11:50 +0200

    add file Profile

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Profile } from './components/Profile/Profile';
+
+
+const App = () => {
+    return (
+        <Profile />
+    );
+}
+
+export default App;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import user from './user.json';
+
+export const Profile = () => {
+    return <div className="profile">
+  <div className="description">
+    <img
+      src={user.avatar}
+      alt={user.name}
+      className="avatar"
+    />
+    <p className="name">{user.name}</p>
+    <p className="tag">{user.tag}</p>
+    <p className="location">{user.location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{user.stats.followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{user.stats.views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{user.stats.likes}</span>
+    </li>
+  </ul>
+</div>
+
+}
