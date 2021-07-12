58c751a267a8aed8ef0b85b48ab5b294b41f207d
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-07-12 20:38:40 +0200

    all right

--- src/components/FriendList/FriendListItem.styles.js
+++ src/components/FriendList/FriendListItem.styles.js
@@ -1,23 +1,7 @@
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
-
-const Online = css`
-    rgb(33, 163, 98);
-`;
-
-const Offline = css`
-    rgb(204, 63, 63);
-`;
-
-const Status = (props) => {
-    if (props.status === false) {
-        return Offline;
-    } 
-    return Online;
-};
 
 export const IsActive = styled.span`
-    border: 0.1875em solid ${Status};
+    border: 0.1875em solid ${props => props.status ? 'rgb(33, 163, 98)' : 'rgb(204, 63, 63)'};
     border-radius: 50%;
     height: 5em;
     width: 5em;
